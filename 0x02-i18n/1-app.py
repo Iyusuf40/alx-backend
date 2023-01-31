9bfd9dd4964cd7e2576d83fd80bc709697569d73
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-31 21:37:37 +0000

    test

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
-""" Module for trying out Babel i18n """
-from flask_babel import Babel
-from flask import Flask, render_template
+""" module's doc str """
 
-app = Flask(__name__, template_folder='templates')
-babel = Babel(app)
+
+from typing import List, Dict, Union, Sequence, Callable, Any
+from flask_babel import Babel
+from flask import Flask, render_template, g, request
+
+
+app = Flask(__name__)
+babel = Babel(app, default_locale='en', default_timezone='UTC')
 
 
 class Config(object):
-    """ Configuration Class for Babel """
-
-    LANGUAGES = ['en', 'fr']
+    """ config for babel """
+    LANGUAGES = ['en', "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object(Config)
+@babel.localeselector
+def get_locale() -> List[str]:
+    """ get_locale func: overrides default """
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
-    """Renders a Basic Template for Babel Implementation"""
-    return render_template("1-index.html")
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ root path """
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
